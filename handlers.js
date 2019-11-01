7dad24199c93e328742cf2c46c12db7c82adf692
Vincent Dörig <dvincdt@gmail.com>
2019-11-01 08:41:39 +0100

    Format files and refactor code

--- handlers.js
+++ handlers.js
@@ -1,56 +1,56 @@
-const quotes = require('./quotes/quotes.json')
+const quotes = require('./quotes/quotes.json');
 
 function randomQuote() {
-  return quotes[Math.floor(Math.random() * quotes.length)]
+  return quotes[Math.floor(Math.random() * quotes.length)];
 }
 
 function quoteById(n) {
-  let quote = Number(n)
+  let quote = Number(n);
   if (quote > quotes.length) {
     return {
-      "error": "Specified number exceeds number of quotes. Total number of quotes right now are " + String(quotes.length) + "."
-    }
+      error: `Specified number exceeds number of quotes. Total number of quotes right now are ${quotes.length}.`,
+    };
   } else if (!Number.isInteger(quote)) {
     return {
-      "error": "That's not a number"
-    }
+      error: "That's not a number",
+    };
   } else if (quote < 1) {
     return {
-      "error": "array starts at 1"
-    }
+      error: 'array starts at 1',
+    };
   } else {
-    return quotes[n - 1]
+    return quotes[n - 1];
   }
 }
 
 function randomQuotes(n) {
-  let data = []
-  let quotesCopy = quotes.slice()
-  let amount = Number(n)
+  let data = [];
+  let quotesCopy = quotes.slice();
+  let amount = Number(n);
   if (amount > quotes.length) {
     return {
-      "error": "Specified number exceeds number of quotes. Total number of quotes right now are " + String(quotes.length) + "."
-    }
+      error: `Specified number exceeds number of quotes. Total number of quotes right now are ${quotes.length}.`,
+    };
   } else if (!Number.isInteger(amount)) {
     return {
-      "error": "That's not a number"
-    }
+      error: "That's not a number",
+    };
   } else if (amount < 1) {
     return {
-      "error": "array starts at 1"
-    }
+      error: 'array starts at 1',
+    };
   } else {
     while (data.length < amount) {
-      let index = Math.floor(Math.random() * quotesCopy.length)
-      data.push(quotesCopy[index])
-      quotesCopy.splice(index, 1)
+      let index = Math.floor(Math.random() * quotesCopy.length);
+      data.push(quotesCopy[index]);
+      quotesCopy.splice(index, 1);
     }
-    return data
+    return data;
   }
 }
 
 function allQuotes() {
-  return quotes
+  return quotes;
 }
 
 module.exports = {
@@ -58,4 +58,4 @@ module.exports = {
   quoteById,
   allQuotes,
   randomQuotes,
-}
+};
--- index.js
+++ index.js
@@ -1,45 +1,45 @@
-const express = require('express')
-const rateLimit = require('express-rate-limit')
-const cors = require('cors')
+const express = require('express');
+const rateLimit = require('express-rate-limit');
+const cors = require('cors');
 const {
   randomQuote,
   quoteById,
   allQuotes,
   randomQuotes,
-} = require('./handlers')
-const app = express()
+} = require('./handlers');
+const app = express();
 
 const limiter = rateLimit({
   windowMs: 10 * 60 * 1000,
   max: 100,
   message: {
-    error: 'Too many requests, please try again later.'
-  }
-})
+    error: 'Too many requests, please try again later.',
+  },
+});
 
-app.enable('trust proxy')
-app.set('json spaces', 4)
-app.use(limiter)
+app.enable('trust proxy');
+app.set('json spaces', 4);
+app.use(limiter);
 
-app.use(cors())
+app.use(cors());
 
-app.use(express.static('views'))
+app.use(express.static('views'));
 
 app.get('/random', (req, res) => {
-  res.json(randomQuote())
-})
+  res.json(randomQuote());
+});
 
 app.get('/random/:amount', (req, res) => {
-  res.json(randomQuotes(req.params.amount))
-})
+  res.json(randomQuotes(req.params.amount));
+});
 
 app.get('/all', (req, res) => {
-  res.json(allQuotes())
-})
+  res.json(allQuotes());
+});
 
 app.get('/quote/:id', (req, res) => {
-  res.json(quoteById(req.params.id))
-})
+  res.json(quoteById(req.params.id));
+});
 
-const port = process.env.PORT || 3333
-app.listen(port, () => console.log(`Server listening on port ${port}!`))
+const port = process.env.PORT || 3333;
+app.listen(port, () => console.log(`Server listening on port ${port}!`));
